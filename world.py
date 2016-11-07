2ab6f812ca96281eb8ea804c71233b77fb1b9356
Elliot Massen <hello@elliotmassen.com>
2016-11-07 23:36:55 +0000

    Creating seperate class file for World

--- world.py
+++ world.py
@@ -1,16 +1,3 @@
-from sys import argv
-from node import Node
-import time
-
-script_name, initial_strength, max_mutation, max_nodes = argv
-initial_strength = int(initial_strength)
-max_mutation = int(max_mutation)
-max_nodes = int(max_nodes)
-
-def print_stats():
-	print "Total nodes: " + str(world.size()) + " | Dead nodes: " + str(world.dead_nodes()) + " | Surviving nodes: " + str(100 - (float(world.dead_nodes()) / float(world.size())) * 100) + "%"
-	print "MAX LIFE: " + str(world.get_max_life())
-
 class World():
 	def __init__(self):
 		self.__nodes = []
@@ -35,7 +22,7 @@ class World():
 	def nodes_are_alive(self):
 		alive = False
 		i = 0
-		while i < world.size() and not alive:
+		while i < self.size() and not alive:
 			if self.__nodes[i].get_life() > 0:
 				alive = True
 			i += 1
@@ -48,20 +35,6 @@ class World():
 	def dead_nodes(self):
 		return self.__graveyard
 
-world = World()
-world.add(Node(initial_strength, max_mutation, world))
-
-i = 0
-while world.size() < max_nodes and world.nodes_are_alive():
-	print "\nROUND " + str(i) + ":"
-	print_stats()
-
-	for n in world.nodes():
-		n.heartbeat()
-
-	i += 1
-	time.sleep(0.5)
-
-print "\nFINAL ROUND:"
-print_stats()
-print ""
+	def print_stats(self):
+		print "Total nodes: " + str(self.size()) + " | Dead nodes: " + str(self.dead_nodes()) + " | Surviving nodes: " + str(100 - (float(self.dead_nodes()) / float(self.size())) * 100) + "%"
+		print "MAX LIFE: " + str(self.get_max_life())
